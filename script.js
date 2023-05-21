2ad9ad4d91ddb21f15ccd7030c6341244bd77918
vickneee <victoria.vavulina@outlook.com>
2023-05-21 23:49:18 +0300

    Corr

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ panels.forEach((panel) => {
 })
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
+    panels.forEach((panel) => {
         panel.classList.remove('active')
     })
 }
